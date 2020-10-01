d218748e660f242fcff0c977d4e83b3267288d88
omrkocar <kocartahir@gmail.com>
2020-09-30 20:53:18 -0400

    Added a bunch of classes in Framework

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,10 +1,16 @@
+#include "hzpch.h"
 #include "Application.h"
+
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Log.h"
+
+#include <GLFW/glfw3.h>
 
 namespace Hazel
 {
 	Application::Application()
 	{
-
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 	Application::~Application()
@@ -14,7 +20,13 @@ namespace Hazel
 
 	void Application::Run()
 	{
-		while (true);
+
+		while (m_Running)
+		{
+			glClearColor(1, 0, 0, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
+		}
 	}
 
 }
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -1,12 +1,17 @@
 #pragma once
 
+
 #ifdef HZ_PLATFORM_WINDOWS
 
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Hazen Engine\n");
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 
